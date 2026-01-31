5a03edf6656254e8f33f93951438a534f30b62c2
mithula2006 <mithulasrajan@gmail.com>
2026-01-31 13:45:48 +0530

    Refactor database connection to use DB_CONFIG

--- app/db.py
+++ app/db.py
@@ -1,11 +1,12 @@
 import psycopg2
-import os
+
+DB_CONFIG = {
+    "dbname": "policy_db",
+    "user": "postgres",
+    "password": "Simk@123",
+    "host": "localhost",
+    "port": 5432
+}
 
 def get_connection():
-    return psycopg2.connect(
-        dbname=os.environ["DB_NAME"],
-        user=os.environ["DB_USER"],
-        password=os.environ["DB_PASSWORD"],
-        host=os.environ["DB_HOST"],
-        port=os.environ["DB_PORT"],
-    )
+    return psycopg2.connect(**DB_CONFIG)
